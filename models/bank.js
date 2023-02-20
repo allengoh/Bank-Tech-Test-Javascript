24ee747b6dc8f2702cc36fad9066233eb62e483d
allengoh <allen1311@hotmail.com>
2023-02-20 18:32:18 +0000

    Written and passed first test for Bank class

---
+++ models/bank.js
@@ -0,0 +1,9 @@
+class Bank {
+  
+  constructor(model, view) {
+    this.model = model;
+    this.view = view;
+  };
+};
+
+module.exports = Bank;
--- tests/bank.test.js
+++ tests/bank.test.js
@@ -1,9 +1,16 @@
 const Bank = require("../models/bank");
 
+jest.mock("../models/bankAccount");
+jest.mock("../models/statement");
+const MockModel = require("../models/bankAccount");
+const MockView = require("../models/statement");
 
 describe("Bank Class", () => {
-  
-  it("constructs", () => {
 
+  it("constructs", () => {
+    const mockModel = new MockModel();
+    const mockView = new MockView();
+    const bank = new Bank(mockModel, mockView);
+    expect(bank).toBeInstanceOf(Bank);
   });
 });
