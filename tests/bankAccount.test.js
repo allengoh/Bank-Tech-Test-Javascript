445b225ff7414895e3e9d33ce62fb691360d9ea7
allengoh <allen1311@hotmail.com>
2023-02-20 12:06:43 +0000

    Written and passed the second test for BankAccount class

--- tests/bankAccount.test.js
+++ tests/bankAccount.test.js
@@ -2,9 +2,16 @@ const BankAccount = require("../models/bankAccount");
 
 describe("Bank Account", () => {
 
-  it("constructs", () => {
+  it("constructs with a balance of 0", () => {
     const bankAccount = new BankAccount();
     expect(bankAccount.balance).toBe(0);
   });
+
+  it("constructs with a balance of 1000", () => {
+    const bankAccount = new BankAccount(1000);
+    expect(bankAccount.balance).toBe(1000);
+  });
+
+  
 
 });
