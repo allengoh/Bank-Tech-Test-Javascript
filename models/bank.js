e6a24be70b6fbb625e1b00e0ff7fb8135e4b9934
allengoh <allen1311@hotmail.com>
2023-02-20 22:35:49 +0000

    Written and passed the fourth test for Bank class

--- models/bank.js
+++ models/bank.js
@@ -6,15 +6,31 @@ class Bank {
   };
 
   deposit(amount, date = new Date()) {
-    let formattedDate = date.toLocaleDateString("en-GB");
     let credit = amount;
     let debit = "";
+    let formattedDate;
+    if(typeof date === "string") {
+      formattedDate = date;
+    } else {
+      let day = date.getDate();
+      let month = date.getMonth() + 1;
+      if( month < 10) {
+        month = `0${date.getMonth() + 1}`
+      } else {
+        month = date.getMonth() + 1;
+      };
+      let year = date.getFullYear();
+      formattedDate = `${day}/${month}/${year}`;
+    };
     this.bankAccount.createTransaction(formattedDate, credit, debit);
     console.log(`The amount of ${amount} has been successfully deposited on ${formattedDate}`);
   };
 
   withdraw(amount, date = new Date()) {
-    let formattedDate = date.toLocaleDateString("en-GB");
+    let formattedDate;
+    if(date){
+      formattedDate = date.toLocaleDateString("en-GB");
+    };
     let credit = "";
     let debit = amount;
     this.bankAccount.createTransaction(formattedDate, credit, debit);
--- tests/bank.test.js
+++ tests/bank.test.js
@@ -26,6 +26,14 @@ describe("Bank Class", () => {
     expect(consoleSpy).toHaveBeenCalledWith("The amount of 100 has been successfully deposited on 20/02/2023");
   });
 
+  it("able to deposit money with date", () => {
+    const mockBankAccount = new BankAccount();
+    const bank = new Bank(mockBankAccount, mockStatement);
+    const consoleSpy = jest.spyOn(console, "log");
+    bank.deposit(500, "10/02/2023");
+    expect(consoleSpy).toHaveBeenCalledWith("The amount of 500 has been successfully deposited on 10/02/2023");
+  });
+
   it("able to withdraw money", () => {
     const mockBankAccount = new BankAccount(1000);
     const bank = new Bank(mockBankAccount, mockStatement);
@@ -33,5 +41,7 @@ describe("Bank Class", () => {
     bank.withdraw(300);
     expect(consoleSpy).toHaveBeenCalledWith("The amount of 300 has been successfully withdrawn on 20/02/2023");
   });
+
+  
 
 });
