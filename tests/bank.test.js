a75f4cb21cacd5f403ccee939915140ca4490097
allengoh <allen1311@hotmail.com>
2023-02-20 18:51:09 +0000

    Updated the variables name for readability

--- tests/bank.test.js
+++ tests/bank.test.js
@@ -2,15 +2,15 @@ const Bank = require("../models/bank");
 
 jest.mock("../models/bankAccount");
 jest.mock("../models/statement");
-const MockModel = require("../models/bankAccount");
-const MockView = require("../models/statement");
+const MockBankAccount = require("../models/bankAccount");
+const MockStatement = require("../models/statement");
 
 describe("Bank Class", () => {
 
   it("constructs", () => {
-    const mockModel = new MockModel();
-    const mockView = new MockView();
-    const bank = new Bank(mockModel, mockView);
+    const mockBankAccount = new MockBankAccount();
+    const mockStatement = new MockStatement();
+    const bank = new Bank(mockBankAccount, mockStatement);
     expect(bank).toBeInstanceOf(Bank);
   });
 });
