5d0a421b0462d880d08b54f2e8228c6a8ae7b9c5
allengoh <allen1311@hotmail.com>
2023-02-20 17:02:55 +0000

    Written and passed second test for Statement class

--- models/statement.js
+++ models/statement.js
@@ -2,7 +2,11 @@ class Statement {
   
   constructor() {
     this.header = "date || credit || debit || balance";
-  }
+  };
+
+  print(statement = "") {
+    return(`${this.header}\n` + `${statement}`);
+  }  
 };
 
 module.exports = Statement;
--- tests/statement.test.js
+++ tests/statement.test.js
@@ -5,6 +5,10 @@ describe("Statement Class", () => {
   it("constructs with a header of a printed bank statement", () => {
     const statement = new Statement();
     expect(statement.header).toBe("date || credit || debit || balance");
-
   });
+  
+  it("returns an empty statement without transactions", () => {
+    const statement = new Statement();
+    expect(statement.print()).toBe("date || credit || debit || balance\n");
+  })
 });
