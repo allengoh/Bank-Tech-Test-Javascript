55975813e4fb285810676e0115add41cebd2c643
allengoh <allen1311@hotmail.com>
2023-02-20 16:50:10 +0000

    Refactored bankAccount.js

--- models/bankAccount.js
+++ models/bankAccount.js
@@ -6,17 +6,17 @@ class BankAccount {
   };
 
   createTransaction(date, credit, debit) {
-      if(Number.isInteger(credit) && debit === "") {
-        this.balance += credit;
-      } else if(Number.isInteger(debit) && credit === "") {
-        if(this.balance === 0) {
-          throw new Error("You have insufficient funds to carry out this transaction.");
-        } else {
-          this.balance -= debit;
-        };
-      } else {
-        throw new Error("Please use an integer for credit or debit."); 
+
+    if(Number.isInteger(credit) && debit === "") {
+      this.balance += credit;
+    } else if(Number.isInteger(debit) && credit === "") {
+      if(this.balance < debit) {
+        throw new Error("You have insufficient funds to carry out this transaction.");
       };
+      this.balance -= debit;
+    } else {
+      throw new Error("Please use an integer for credit or debit."); 
+    };
 
     let transaction = {
       date: date,
