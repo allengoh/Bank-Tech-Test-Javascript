4b688d6ecdbc5ceb2522e2a8bdd6c455b42804fc
allengoh <allen1311@hotmail.com>
2023-02-20 18:57:05 +0000

    Updated the bank.test.js again for readability

--- tests/bank.test.js
+++ tests/bank.test.js
@@ -1,16 +1,19 @@
 const Bank = require("../models/bank");
 
+//We are mocking BankAccount and Statement class here
+const BankAccount = require("../models/bankAccount");
+const Statement = require("../models/statement");
 jest.mock("../models/bankAccount");
 jest.mock("../models/statement");
-const MockBankAccount = require("../models/bankAccount");
-const MockStatement = require("../models/statement");
+
+const mockBankAccount = new BankAccount();
+const mockStatement = new Statement();
 
 describe("Bank Class", () => {
 
   it("constructs", () => {
-    const mockBankAccount = new MockBankAccount();
-    const mockStatement = new MockStatement();
     const bank = new Bank(mockBankAccount, mockStatement);
     expect(bank).toBeInstanceOf(Bank);
   });
+
 });
