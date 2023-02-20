825963a57ead3c3ff4f5b62c2e6311da89c737e4
allengoh <allen1311@hotmail.com>
2023-02-20 19:36:52 +0000

    Written and passed second test for Bank class

--- models/bank.js
+++ models/bank.js
@@ -1,9 +1,17 @@
 class Bank {
   
   constructor(model, view) {
-    this.model = model;
-    this.view = view;
+    this.bankAccount = model;
+    this.statement = view;
   };
+
+  deposit(amount, date = new Date()) {
+    let formattedDate = date.toLocaleDateString("en-GB");
+    let credit = amount;
+    let debit = "";
+    this.bankAccount.createTransaction(formattedDate, credit, debit);
+    console.log(`The amount of ${amount} has been successfully deposited on ${formattedDate}`);
+  }
 };
 
 module.exports = Bank;
--- tests/bank.test.js
+++ tests/bank.test.js
@@ -3,8 +3,10 @@ const Bank = require("../models/bank");
 //We are mocking BankAccount and Statement class here
 jest.mock("../models/bankAccount");
 jest.mock("../models/statement");
+
 const BankAccount = require("../models/bankAccount");
 const Statement = require("../models/statement");
+
 const mockBankAccount = new BankAccount();
 const mockStatement = new Statement();
 
@@ -16,7 +18,10 @@ describe("Bank Class", () => {
   });
 
   it("able to deposit money", () => {
-
+    const bank = new Bank(mockBankAccount, mockStatement);
+    const consoleSpy = jest.spyOn(console, "log");
+    bank.deposit(100);
+    expect(consoleSpy).toHaveBeenCalledWith("The amount of 100 has been successfully deposited on 20/02/2023");
   });
 
 });
