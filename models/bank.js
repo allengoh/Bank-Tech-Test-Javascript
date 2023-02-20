b051db8968b2e19842404215e3bcfea0b8886621
allengoh <allen1311@hotmail.com>
2023-02-20 22:59:22 +0000

    Refactored withdraw function of bank class

--- models/bank.js
+++ models/bank.js
@@ -19,18 +19,26 @@ class Bank {
       };
       month = `${month}`.padStart(2, '0');
       formattedDate = `${day}/${month}/${year}`;
-    }
+    };
     this.bankAccount.createTransaction(formattedDate, credit, debit);
     console.log(`The amount of ${amount} has been successfully deposited on ${formattedDate}`);
   };
 
   withdraw(amount, date = new Date()) {
-    let formattedDate;
-    if(date){
-      formattedDate = date.toLocaleDateString("en-GB");
-    };
     let credit = "";
     let debit = amount;
+    let formattedDate;
+    if (typeof date === "string") {
+      formattedDate = date;
+    } else {
+      let { day, month, year } = {
+        day: date.getDate(),
+        month: date.getMonth() + 1,
+        year: date.getFullYear(),
+      };
+      month = `${month}`.padStart(2, '0');
+      formattedDate = `${day}/${month}/${year}`;
+    };
     this.bankAccount.createTransaction(formattedDate, credit, debit);
     console.log(`The amount of ${amount} has been successfully withdrawn on ${formattedDate}`);
   };
