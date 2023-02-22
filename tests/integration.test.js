5be36a4e9de838294f532605867ee3944e666783
allengoh <allen1311@hotmail.com>
2023-02-22 23:00:59 +0000

    Created an integration test file to test for acceptance criteria

---
+++ tests/integration.test.js
@@ -0,0 +1,8 @@
+const Bank = require("../models/bank");
+const BankAccount = require("../models/bankAccount");
+const Statement = require("../models/statement");
+
+
+describe("Integration test for bank class", () => {
+  
+});
